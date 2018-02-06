7fc9f96e3532603db83ce882d193556f0dd27444
Detestedwharf16 <Hbrock2019@gmail.com>
2018-02-05 19:21:13 -0800

    Add files via upload

--- BarcodeTests.py
+++ BarcodeTests.py
@@ -1,32 +1,40 @@
 import csv
 
+
 names = []
 timeIn = []
 timeOut = []
 ui = input("Who are you looking for?")
 testdex = []
+TimeOutput1 = []
+TimeOutput2 = []
 
-with open('untitled.csv') as csvfile:
+with open('Untitled.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
-        names.append(row[1])
-        timeIn.append(row[2])
-        timeOut.append(row[3])
+        names.append(row[0])
+        timeIn.append(row[1])
+        timeOut.append(row[2])
+
+
+def times(name):
+    for i in range(len(timeIn)):
+        if names[i] == name:
+            print('In:', timeIn[i])
+            TimeOutput1.append(timeIn[i])
+            print('Out:', timeOut[i])
+            TimeOutput2.append(timeOut[i])
 
 
 
+times(ui)
 
-for index, value in enumerate(names):
-    # If the current value matches something, append the index to the list
-    if value == ui:
-        testdex.append(index)
-
-
-search = timeIn[len(testdex)]
-search2 = timeOut[len(testdex)]
-
-
+with open('Output.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, dialect='excel', delimiter = ' ')
+    writer.writerow([ui])
+    for i in range(len(TimeOutput1)):
+        writer.writerow([TimeOutput1[i]])
+        writer.writerow([TimeOutput2[i]])
+    #writer.writerow([TimeOutput1])
 
 
-print(testdex)
-print(ui,"entered at", search, "and left at", search2)
